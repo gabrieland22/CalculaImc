7ffc5654ab6138cb06e89d869569c4444bc8c33c
Gabriel <gabrieland22@gmail.com>
2018-09-17 23:23:05 -0300

    Classes Domain

---
+++ CalculaImc/CalculaImc.Domain.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CalculaImc.Domain.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ CalculaImc/CalculaImc.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CalculaImc.Domain
+{
+    public class Class1
+    {
+    }
+}
